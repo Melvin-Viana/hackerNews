5a96cc72776cd8540f0fadc6dfd5397b99189788
melvin-viana <mjviana057@gmail.com>
2020-05-12 21:21:51 -0700

    Setup webpack/babel w/ react

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = props => <h2>Hey</h2>;
+
+ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/App.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
