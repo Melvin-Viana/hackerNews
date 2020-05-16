b6ae5a707a9ee95f0676ca4f82ed3e268e5cb130
melvin-viana <mjviana057@gmail.com>
2020-05-15 17:06:17 -0700

    Add Header elements

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hacker News Search
+> Fetching data from Hacker News and displaying in a Mobile Responsive app
+
+Tech Stack:
+- React
+- HTML
+- CSS
+- Webpack + Babel
+- [Algolia Search's API](https://hn.algolia.com/api)
--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-const App = props => <h2>Hey</h2>;
+const App = props => {
+  useEffect(()=>{
+    axios.get('http://hn.algolia.com/api/v1/search?tags=front_page')
+    .then(data=>{
+      console.log(data);
+    })
+  },[]);
+  return(<h2>Hey</h2>);
+};
 
 ReactDOM.render(<App/>, document.getElementById('app'));
+
+
