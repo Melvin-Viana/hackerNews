09daf254f2742a2eec3a57f5fc088988a5fa8535
melvin-viana <mjviana057@gmail.com>
2020-06-08 20:52:02 -0700

    Add comments for documentation purposes

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ import {getDateFilter, getTagsFilter} from './helpers/SearchFilters';
 import { useDebounce } from 'use-debounce';
 
 const App = props => {
-
+  // Hooks for state
   const [selectedPost, setPost] = useState('Stories')
   const [selectedSort, setSort] = useState('Popularity');
   const [selectedTime, setTime] = useState('All Time')
   const [currentSearchResults, setResults] = useState([]);
   const [numPages, setNumPages] = useState(0);
   const [activePage, setPage] = useState(1);
-  const [searchQuery, setQuery] = useState('')
+  const [searchQuery, setQuery] = useState('');
+  // Track previous values
   const previousVals = {
     prevPost: usePrevious(selectedPost),
     prevTime: usePrevious(selectedTime),
@@ -24,50 +25,54 @@ const App = props => {
     prevSearch:usePrevious(searchQuery)
   }
 
+  // Reusable function for tracking changed state w/ hooks
   const hasChanged = (prevVal, current) =>
   prevVal!== undefined&&prevVal!==current;
   const [debouncedQuery] = useDebounce(searchQuery,800);
 
-  useEffect( ()=>{
+  useEffect(() => {
     const {prevPost, prevSort, prevTime, prevSearch} = previousVals;
     // Previous values hasChanged
     let sort = hasChanged(prevSort, selectedSort);
     let time = hasChanged(prevTime, selectedTime);
     let post = hasChanged(prevPost, selectedPost);
     let query = hasChanged(prevSearch, debouncedQuery);
-
+    // Sets page to 1 when activePage is not one and a get request is made
     if(activePage !== 1 &&(sort || time || post || query)) {
       return setPage(1);
     }
+
+    // Get filters to be added to the get request
     let tags = getTagsFilter(selectedPost);
-    let currQuery = debouncedQuery !== '' ? '&query='+debouncedQuery : '';
-    console.log('hey');
     let date = getDateFilter(selectedTime);
+    let currQuery = debouncedQuery !== '' ? '&query='+ debouncedQuery : '';
+
     const fetchData = async (searchBy) => {
       const res = await axios.get(`http://hn.algolia.com/api/v1/${searchBy}?tags=${tags}${currQuery}&hitsPerPage=30&page=${activePage-1}${date}`);
       setResults(res.data.hits);
       setNumPages(res.data.nbPages);
     }
+    // If second dropdown is "Popularity"
     if(selectedSort === "Popularity"){
       fetchData('search');
     } else {
       fetchData('search_by_date');
     }
 
-  },[activePage,selectedSort,selectedPost,selectedTime, debouncedQuery]);
+  },[activePage,selectedSort,selectedPost,selectedTime, debouncedQuery]); // Detects changes within the following hooks
   return(
   <React.Fragment>
+    {/* TODO: Make a component for this */}
     <div class="header">
-        <i class="fa fa-h-square fa-3x"></i>
-        <h2>HackerNews</h2>
-        <div class="search">
-          <i class="fa fa-search fa-2x"></i>
-          <input type="text" placeholder="Search stories by author, title, url or author" onChange={e=>setQuery(e.target.value)} value={searchQuery}/>
-        </div>
-        <i class="fa fa-cog fa-2x"></i>
-        <h3>Settings</h3>
-
+      <i class="fa fa-h-square fa-3x"></i>
+      <h2>HackerNews</h2>
+      <div class="search">
+        <i class="fa fa-search fa-2x"></i>
+        <input type="text" placeholder="Search stories by author, title, url or author" onChange={e=>setQuery(e.target.value)} value={searchQuery}/>
       </div>
+      <i class="fa fa-cog fa-2x"></i>
+      <h3>Settings</h3>
+    </div>
   <DropDownContainer
     handleTimeChange={setTime}
     selectedTime={selectedTime}
@@ -89,5 +94,3 @@ const App = props => {
 };
 
 ReactDOM.render(<App/>, document.getElementById('app'));
-
-
